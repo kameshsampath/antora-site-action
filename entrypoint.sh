a484cda2f8eba0d745618e989fe28f3a368e8c96
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-21 19:14:36 +0530

    change path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,10 @@ set
 set -eu 
 set -o pipefail 
 
-set -x
-
 echo "Using $INPUT_SITE_SOURCES_PATH"
 
-cd $RUNNER_WORKSPACE/$INPUT_SITE_SOURCES_PATH
+cd $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 
-set +x 
+ls -ltr $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 
 antora --stacktrace $INPUT_ANTORA_PLAYBOOK

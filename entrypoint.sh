6401174500ad3f4cc0cf182bc58f2976b4aa2f0b
Ben Franske <ben.mm@franske.com>
2022-01-21 12:17:17 -0600

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,14 @@ echo "Using args:
  docsearch enabled: $INPUT_ANTORA_LUNR_DOCSEARCH_ENABLED
  "
 
-args=()
+#args=()
 
-args +=( '--stacktrace' )
+#args +=( '--stacktrace' )
 
-if [ "$INPUT_ANTORA_LUNR_DOCSEARCH_ENABLED" = true ] ; then
-	npm install @antora/lunr-extension
-	args +=( '--extension @antora/lunr-extension' )
-fi
+#if [ "$INPUT_ANTORA_LUNR_DOCSEARCH_ENABLED" = true ] ; then
+#	npm install @antora/lunr-extension
+#	args +=( '--extension @antora/lunr-extension' )
+#fi
 
 pwd
 
@@ -27,4 +27,4 @@ cd $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 ls -ltr $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 
 NODE_PATH="$(npm -g root)" \
-antora "${args[@]}" generate $INPUT_ANTORA_PLAYBOOK
+#antora "${args[@]}" generate $INPUT_ANTORA_PLAYBOOK
